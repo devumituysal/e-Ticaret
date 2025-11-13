b8756da93f776ca1acbacec4b397ea50293dc1ca
devumituysal <dev.umituysal@gmail.com>
2025-11-13 22:21:12 +0300

    App.Admin eklendi
    App.Admin mvc eklendi
    Admin ve e-Ticaret projelerine gerekli controllerlar ve actionları eklendi.
    ilgili actionların view dosyaları oluşturuldu.

--- App/App.e-Ticaret/Controllers/HomeController.cs
+++ App/App.e-Ticaret/Controllers/HomeController.cs
@@ -1,23 +1,42 @@
-using System.Diagnostics;
-using App.e_Ticaret.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.e_Ticaret.Controllers
 {
     public class HomeController : Controller
     {
-
-
         public IActionResult Index()
         {
             return View();
         }
-
-        [Route("/products/list")]
+        [Route("/about-us")]
+        [HttpGet]
+        public IActionResult AboutUs()  
+        {
+            return View();
+        }
+        [Route("/contact")]
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [Route("/contact")]
+        [HttpPost]
+        public IActionResult Contact([FromForm] object newContactMessageModel)
+        {
+            return View();
+        }
+        [Route("/product/list")]
+        [HttpGet]
         public IActionResult Listing()
         {
             return View();
         }
-
+        [Route("/product/{productId:int}/details")]
+        [HttpGet]
+        public IActionResult ProductDetail([FromRoute] int productId)
+        {
+            return View();
+        }
     }
 }
--- App/App.e-Ticaret/Models/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace App.e_Ticaret.Models
-{
-    public class Class
-    {
-    }
-}
